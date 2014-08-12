1cc70df44b4bf45eb33f378d3a29dd5e7829b4e9
itkg-bfouche <benjamin.fouche@businessdecision.com>
2014-08-12 11:09:50 +0200

    add routing in displayBundle

---
+++ DisplayBundle/Routing/PhpOrchestraRouter.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace PHPOrchestra\DisplayBundle\Routing;
+
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * The FrameworkBundle router is extended to inject documents service
+ * in PhpOrchestraUrlMatcher
+ */
+class PhpOrchestraRouter extends Router
+{
+    protected $cacheService;
+    protected $nodeRepository;
+
+    /**
+     * Extends parent constructor to get documents service
+     * as $container is private in parent class
+     *  
+     * @param ContainerInterface $container
+     * @param mixed              $resource
+     * @param array              $options
+     * @param RequestContext     $context
+     */
+    public function __construct(
+        ContainerInterface $container,
+        $resource,
+        array $options = array(),
+        RequestContext $context = null
+    )
+    {
+        parent::__construct($container, $resource, $options, $context);
+
+        $this->cacheService = $container->get('php_orchestra_base.cache_manager');
+        $this->nodeRepository = $container->get('php_orchestra_model.repository.node');
+    }
+
+    /**
+     * Override parent getMatcher to inject documents service
+     * in PhpOrchestraUrlMatcher
+     *
+     * @return UrlMatcherInterface|null
+     */
+    public function getMatcher()
+    {
+        if (null !== $this->matcher) {
+            return $this->matcher;
+        }
+
+        return $this->matcher = new $this->options['matcher_class'](
+            $this->getRouteCollection(),
+            $this->context,
+            $this->nodeRepository,
+            $this->cacheService
+        );
+    }
+
+    /**
+     * Get the url generator
+     *
+     * @return UrlGeneratorInterface|null
+     */
+    public function getGenerator()
+    {
+        if (null !== $this->generator) {
+            return $this->generator;
+        }
+
+        return $this->generator = new $this->options['generator_class'](
+            $this->getRouteCollection(),
+            $this->context,
+            $this->nodeRepository,
+            $this->logger
+        );
+    }
+}
