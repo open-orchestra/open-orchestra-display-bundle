c6747f6e6c0445408fb62f91d65b35a74eb3a015
itkg-bfouche <benjamin.fouche@businessdecision.com>
2014-11-05 16:15:46 +0100

    create a block content_list

---
+++ DisplayBundle/DisplayBlock/Strategies/ContentListStrategy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace PHPOrchestra\DisplayBundle\DisplayBlock\Strategies;
+
+use PHPOrchestra\DisplayBundle\DisplayBlock\DisplayBlockInterface;
+use PHPOrchestra\ModelBundle\Model\BlockInterface;
+use PHPOrchestra\ModelBundle\Repository\ContentRepository;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class ContentListStrategy
+ */
+class ContentListStrategy extends AbstractStrategy
+{
+    protected $contentRepository;
+
+    /**
+     * @param ContentRepository $contentRepository
+     */
+    public function __construct(ContentRepository $contentRepository)
+    {
+       $this->contentRepository = $contentRepository;
+    }
+
+    /**
+     * Check if the strategy support this block
+     *
+     * @param BlockInterface $block
+     *
+     * @return boolean
+     */
+    public function support(BlockInterface $block)
+    {
+        return DisplayBlockInterface::CONTENT_LIST == $block->getComponent();
+    }
+
+    /**
+     * Perform the show action for a block
+     *
+     * @param BlockInterface $block
+     *
+     * @return Response
+     */
+    public function show(BlockInterface $block)
+    {
+        $attributes = $block->getAttributes();
+        $contents = $this->contentRepository->findByContentType();
+
+        return $this->render(
+            'PHPOrchestraDisplayBundle:Block/ContentList:show.html.twig',
+            array(
+                'contents' => $contents
+            )
+        );
+    }
+
+    /**
+     * Get the name of the strategy
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'content_list';
+    }
+} 
