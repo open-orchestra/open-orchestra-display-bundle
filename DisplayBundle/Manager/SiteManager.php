d67c6181183d6e174d7488f4253e33b44742cd1b
Nicolas Thal <nico.th4l@gmail.com>
2014-11-12 10:59:48 +0100

    complete site manager

--- DisplayBundle/Manager/SiteManager.php
+++ DisplayBundle/Manager/SiteManager.php
@@ -3,6 +3,8 @@
 namespace PHPOrchestra\DisplayBundle\Manager;
 
 use PHPOrchestra\BaseBundle\Context\CurrentSiteIdInterface;
+use PHPOrchestra\ModelBundle\Model\SiteInterface;
+use PHPOrchestra\ModelBundle\Repository\SiteRepository;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
@@ -10,18 +12,18 @@ use Symfony\Component\HttpFoundation\RequestStack;
  */
 class SiteManager implements CurrentSiteIdInterface
 {
-    /**
-     * @var string
-     */
     protected $siteId;
     protected $requestStack;
+    protected $siteRepository;
 
     /**
-     * @param RequestStack $requestStack
+     * @param RequestStack   $requestStack
+     * @param SiteRepository $siteRepository
      */
-    public function __construct(RequestStack $requestStack)
+    public function __construct(RequestStack $requestStack, SiteRepository $siteRepository)
     {
         $this->requestStack = $requestStack;
+        $this->siteRepository = $siteRepository;
         $request = $this->requestStack->getCurrentRequest();
 
         if (!is_null($request)) {
@@ -45,5 +47,18 @@ class SiteManager implements CurrentSiteIdInterface
     public function setSiteId($siteId)
     {
         $this->siteId = $siteId;
+    }
+
+    /**
+     * Get the current default language of the current site
+     *
+     * @return string
+     */
+    public function getCurrentSiteDefaultLanguage()
+    {
+        /** @var SiteInterface $site */
+        $site = $this->siteRepository->findBySiteId($this->getCurrentSiteId());
+
+        return $site->getDefaultLanguage();
     }
 }
