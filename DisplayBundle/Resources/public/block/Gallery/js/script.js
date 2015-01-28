31c35f846b5198cb5d0892abfa5e02f9d518ae5f
itkg-ngilain <noel.gilain@businessdecision.com>
2015-01-28 18:37:37 +0100

    First use of require.js

--- DisplayBundle/Resources/public/block/Gallery/js/script.js
+++ DisplayBundle/Resources/public/block/Gallery/js/script.js
@@ -1,33 +1,41 @@
-function resizeThumbnails(galId, nbCol) {
-    if (galId == '') alert('Error : no id defined for gallery');
-    var picturePadding = parseInt($(".gallery-picture").css("border-left-width")) + parseInt($(".gallery-picture").css("margin-left"));
-    var galleryWidth = parseInt($("#" + galId).width());
-    var pictureWidth = parseInt((galleryWidth / nbCol) - 2*picturePadding);
+requirejs(
+    ['../../../fancybox/source/jquery.fancybox.pack',
+     '../../../fancybox/source/helpers/jquery.fancybox-buttons',
+     '../../../fancybox/source/helpers/jquery.fancybox-media',
+     '../../../fancybox/source/helpers/jquery.fancybox-thumbs'
+    ],
+    function () {
 
-    $("#" + galId + " .gallery-picture").width(pictureWidth + 'px');
-    $("#" + galId + " .gallery-picture").css("visibility", "visible");
-}
+        function resizeThumbnails(galId, nbCol) {
+            if (galId == '') alert('Error : no id defined for gallery');
+            var picturePadding = parseInt($(".gallery-picture").css("border-left-width")) + parseInt($(".gallery-picture").css("margin-left"));
+            var galleryWidth = parseInt($("#" + galId).width());
+            var pictureWidth = parseInt((galleryWidth / nbCol) - 2*picturePadding);
 
-$(document).ready(function() {
-
-    alert('/!\\ Pause temporaire pour laisser le temps aux js de charger => A remplacer par une solution de configuration générale des js /!\\')
-
-    for(var galleryId in orchestraGalCol) {
-        resizeThumbnails(galleryId, orchestraGalCol[galleryId]);
-    }
-
-    $(".fancybox-thumb").fancybox({
-        prevEffect  : 'none',
-        nextEffect  : 'none',
-        closeBtn    : false,
-        helpers     : {
-            title     : {
-                type    : 'inside'
-            },
-            thumbs    : {
-                width   : 50,
-                height  : 50
-            }
+            $("#" + galId + " .gallery-picture").width(pictureWidth + 'px');
+            $("#" + galId + " .gallery-picture").css("visibility", "visible");
         }
-    });
-});
+
+        $(document).ready(function() {
+            for(var galleryId in orchestraGalCol) {
+                resizeThumbnails(galleryId, orchestraGalCol[galleryId]);
+            }
+
+            $(".fancybox-thumb").fancybox({
+                prevEffect  : 'none',
+                nextEffect  : 'none',
+                closeBtn    : false,
+                helpers     : {
+                    title     : {
+                        type    : 'inside'
+                    },
+                    thumbs    : {
+                        width   : 50,
+                        height  : 50
+                    }
+                }
+            });
+        });
+
+    }
+);
