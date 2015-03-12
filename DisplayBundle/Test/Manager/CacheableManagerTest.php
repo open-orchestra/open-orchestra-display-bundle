d7b0329d563bd9399d356555f4ee5a38b8c3294d
itkg-ngilain <noel.gilain@businessdecision.com>
2015-03-12 18:10:00 +0100

    Fix CacheableManager broken tests

--- DisplayBundle/Test/Manager/CacheableManagerTest.php
+++ DisplayBundle/Test/Manager/CacheableManagerTest.php
@@ -26,31 +26,48 @@ class CacheableManagerTest extends \PHPUnit_Framework_TestCase
     /**
      * @param int $maxAge
      * @param int $expectedMaxAge
-     * @param int $count
+     * @param int count
      *
-     * @dataProvider provideMaxAgeAndCount
+     * @dataProvider provideMaxAge
      */
-    public function testSetMaxAge($maxAge, $expectedMaxAge, $count = 1)
+    public function testSetPublicResponseCacheParameters($maxAge, $expectedMaxAge, $count)
     {
         $response = Phake::mock('Symfony\Component\HttpFoundation\Response');
 
-        $newResponse = $this->manager->setMaxAge($maxAge, $response);
+        $newResponse = $this->manager->setResponseCacheParameters($response, $maxAge, 'public');
 
         $this->assertSame($response, $newResponse);
+        Phake::verify($newResponse)->setPublic();
+        Phake::verify($newResponse, Phake::times($count))->setMaxAge($expectedMaxAge);
+        Phake::verify($newResponse, Phake::times($count))->setSharedMaxAge($expectedMaxAge);
+    }
+
+    /**
+     * @param int $maxAge
+     * @param int $expectedMaxAge
+     *
+     * @dataProvider provideMaxAge
+     */
+    public function testSetPrivateResponseCacheParameters($maxAge, $expectedMaxAge, $count)
+    {
+        $response = Phake::mock('Symfony\Component\HttpFoundation\Response');
+
+        $newResponse = $this->manager->setResponseCacheParameters($response, $maxAge, 'private');
+
+        $this->assertSame($response, $newResponse);
+        Phake::verify($newResponse)->setPrivate();
         Phake::verify($newResponse, Phake::times($count))->setMaxAge($expectedMaxAge);
     }
 
     /**
      * @return array
      */
-    public function provideMaxAgeAndCount()
+    public function provideMaxAge()
     {
         return array(
-            array(1, 1),
-            array(1000, 1000),
-            array(5, 5),
-            array(0, Phake::anyParameters(), 0),
-            array(-1, 2629743),
+            array(300, 300, 1),
+            array(-1, 2629743, 1),
+            array(0, 0, 0)
         );
     }
 }
