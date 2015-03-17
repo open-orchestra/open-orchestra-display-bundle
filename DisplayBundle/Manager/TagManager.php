01711c9f01d19024b8cb6c1fcf5195ad580a049c
itkg-ngilain <noel.gilain@businessdecision.com>
2015-03-17 17:32:59 +0100

    Document TagManager

--- DisplayBundle/Manager/TagManager.php
+++ DisplayBundle/Manager/TagManager.php
@@ -7,36 +7,85 @@ namespace OpenOrchestra\DisplayBundle\Manager;
  */
 class TagManager
 {
+    /**
+     * Format node id tag
+     * 
+     * @param string $nodeId
+     * 
+     * return string
+     */
     public function formatNodeIdTag($nodeId)
     {
         return 'node-' . $nodeId;
     }
 
+    /**
+     * Format language tag
+     * 
+     * @param string $language
+     * 
+     * return string
+     */
     public function formatLanguageTag($language)
     {
         return 'language-' . $language;
     }
 
+    /**
+     * Format site id tag
+     * 
+     * @param string $siteId
+     * 
+     * return string
+     */
     public function formatSiteIdTag($siteId)
     {
         return 'site-' . $siteId;
     }
 
+    /**
+     * Format block type tag
+     * 
+     * @param string $blockType
+     * 
+     * return string
+     */
     public function formatBlockTypeTag($blockType)
     {
         return 'block-' . $blockType;
     }
 
+    /**
+     * Format content type tag
+     * 
+     * @param string $contentType
+     * 
+     * return string
+     */
     public function formatContentTypeTag($contentType)
     {
         return 'contentType-' . $contentType;
     }
 
+    /**
+     * Format content id tag
+     * 
+     * @param string $contentId
+     * 
+     * return string
+     */
     public function formatContentIdTag($contentId)
     {
         return 'contentId-' . $contentId;
     }
 
+    /**
+     * Format media id tag
+     * 
+     * @param string $mediaId
+     * 
+     * return string
+     */
     public function formatMediaId($mediaId)
     {
         return 'mediaId-' . $mediaId;
