85fb4560f8664d54df2f9b88ea3fecf9dd00e1ad
itkg-ngilain <noel.gilain@businessdecision.com>
2015-03-20 17:28:12 +0100

    Update SubMenu

--- DisplayBundle/DisplayBlock/Strategies/SubMenuStrategy.php
+++ DisplayBundle/DisplayBlock/Strategies/SubMenuStrategy.php
@@ -9,6 +9,7 @@ use OpenOrchestra\ModelInterface\Repository\NodeRepositoryInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use OpenOrchestra\BaseBundle\Manager\TagManager;
 
 /**
  * Class SubMenuStrategy
@@ -18,17 +19,24 @@ class SubMenuStrategy extends AbstractStrategy
     protected $nodeRepository;
     protected $router;
     protected $request;
+    protected $tagManager;
 
     /**
      * @param NodeRepositoryInterface $nodeRepository
      * @param UrlGeneratorInterface   $router
      * @param RequestStack            $requestStack
+     * @param TagManager              $tagManager
      */
-    public function __construct(NodeRepositoryInterface $nodeRepository, UrlGeneratorInterface $router, RequestStack $requestStack)
-    {
+    public function __construct(
+        NodeRepositoryInterface $nodeRepository,
+        UrlGeneratorInterface $router,
+        RequestStack $requestStack,
+        TagManager $tagManager
+    ){
         $this->nodeRepository = $nodeRepository;
         $this->router = $router;
         $this->request = $requestStack->getCurrentRequest();
+        $this->tagManager = $tagManager;
     }
 
     /**
@@ -44,6 +52,16 @@ class SubMenuStrategy extends AbstractStrategy
     }
 
     /**
+     * Indicate if the block is public or private
+     * 
+     * @return boolean
+     */
+    public function isPublic(BlockInterface $block)
+    {
+        return true;
+    }
+
+    /**
      * Perform the show action for a block
      *
      * @param BlockInterface $block
@@ -54,15 +72,9 @@ class SubMenuStrategy extends AbstractStrategy
      */
     public function show(BlockInterface $block)
     {
-        $nodes = null;
-        $nodeName = $block->getAttribute('nodeName');
-
-        if (!is_null($nodeName)) {
-            $nodes = $this->nodeRepository->getSubMenu($nodeName, $block->getAttribute('nbLevel'), $this->request->getLocale());
-        }
+        $nodes = $this->getNodes($block);
 
         if (!is_null($nodes)) {
-
             return $this->render(
                 'OpenOrchestraDisplayBundle:Block/Menu:tree.html.twig',
                 array(
@@ -73,7 +85,48 @@ class SubMenuStrategy extends AbstractStrategy
             );
         }
 
-        throw new NodeNotFoundException($nodeName);
+        throw new NodeNotFoundException($block->getAttribute('nodeName'));
+    }
+
+    /**
+     * Get nodes to display
+     * 
+     * @param BlockInterface $block
+     *
+     * @return array
+     */
+    protected function getNodes(BlockInterface $block)
+    {
+        $nodes = null;
+        $nodeName = $block->getAttribute('nodeName');
+
+        if (!is_null($nodeName)) {
+            $nodes = $this->nodeRepository->getSubMenu($nodeName, $block->getAttribute('nbLevel'), $this->request->getLocale());
+        }
+
+        return $nodes;
+    }
+
+    /**
+     * Return block specific tags
+     * 
+     * @param BlockInterface $block
+     * 
+     * @return array
+     */
+    public function getTags(BlockInterface $block)
+    {
+        $tags = array();
+
+        $nodes = $this->getNodes($block);
+
+        if ($nodes) {
+            foreach ($nodes as $node) {
+                $tags[] = $this->tagManager->formatNodeIdTag($node->getNodeId());
+            }
+        }
+
+        return $tags;
     }
 
     /**
