8a17062bad8f16bd397db4ba4dd48a45071f371b
itkg-alavieille <amaury.lavieille@businessdecision.com>
2015-04-14 09:49:29 +0200

    fix space

--- DisplayBundle/DependencyInjection/OpenOrchestraDisplayExtension.php
+++ DisplayBundle/DependencyInjection/OpenOrchestraDisplayExtension.php
@@ -18,7 +18,7 @@ class OpenOrchestraDisplayExtension extends Extension
      * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
-    {   
+    {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
