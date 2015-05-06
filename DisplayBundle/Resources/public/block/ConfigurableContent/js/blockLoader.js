e3f254a5f88d3788326b942d893e86b859af9fe5
itkg-ngilain <noel.gilain@businessdecision.com>
2015-05-06 15:05:44 +0200

    Update ConfigurableContent front loading

---
+++ DisplayBundle/Resources/public/block/ConfigurableContent/js/blockLoader.js
@@ -0,0 +1,4 @@
+require(['openOrchestraCss'], 
+    function(openOrchestraCss) {
+        openOrchestraCss.load(['/bundles/openorchestradisplay/block/ConfigurableContent/css/style.css']);
+});
--- DisplayBundle/Resources/public/block/ConfigurableContent/js/loadCss.js
+++
@@ -1,6 +0,0 @@
-requirejs(
-    ['/bundles/openorchestradisplay/libs/requireCss.js'],
-    function() {
-        loadCss('/bundles/openorchestradisplay/block/ConfigurableContent/css/style.css');
-    }
-);
