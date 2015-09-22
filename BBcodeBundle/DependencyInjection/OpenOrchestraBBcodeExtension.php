df06b6e625e8515c8dc488ca0dd53db0cb98fdf1
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-09-22 14:32:29 +0200

    remove unused parameter

--- BBcodeBundle/DependencyInjection/OpenOrchestraBBcodeExtension.php
+++ BBcodeBundle/DependencyInjection/OpenOrchestraBBcodeExtension.php
@@ -17,13 +17,13 @@ class OpenOrchestraBBcodeExtension extends Extension
     /**
      * Loads a specific configuration.
      *
-     * @param array            $config    An array of configuration values
+     * @param array            $configs    An array of configuration values
      * @param ContainerBuilder $container A ContainerBuilder instance
      */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('bbcode.yml');
