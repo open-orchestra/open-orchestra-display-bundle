1e2657ecb893667f5d7b411c48fd50f9326d351f
Nicolas Thal <nico.th4l@gmail.com>
2015-10-06 14:48:28 +0200

    update node repository method names

--- DisplayBundle/DisplayBlock/Strategies/ContentListStrategy.php
+++ DisplayBundle/DisplayBlock/Strategies/ContentListStrategy.php
@@ -95,7 +95,7 @@ class ContentListStrategy extends AbstractStrategy
             if ('' != $block->getAttribute('contentNodeId')) {
                 $language = $this->currentSiteManager->getCurrentSiteDefaultLanguage();
                 $siteId = $this->currentSiteManager->getCurrentSiteId();
-                $parameters['contentNodeId'] = $this->nodeRepository->findOnePublishedByNodeIdAndLanguageAndSiteIdInLastVersion($block->getAttribute('contentNodeId'), $language, $siteId)->getId();
+                $parameters['contentNodeId'] = $this->nodeRepository->findPublishedInLastVersion($block->getAttribute('contentNodeId'), $language, $siteId)->getId();
             }
 
             return $this->render('OpenOrchestraDisplayBundle:Block/ContentList:show.html.twig', $parameters);
