9b8eecb305d7af7c64bce75a08ef2e5aa07c3833
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-03-03 15:16:08 +0100

    PSR2

--- DisplayBundle/Resources/public/block/Contact/blockLoader.js
+++ DisplayBundle/Resources/public/block/Contact/blockLoader.js
@@ -1,19 +1,19 @@
 require(
     ['jquery'],
-	function (OpenOrchestraCss) {
-		$(document).ready(function(){
-			$('form[name="Contact"').submit(function(event) {
-	            event.preventDefault();
-	            var data = $(this).serialize();   
-	            $.ajax({
-	                url: $(this).attr('action'),
-	                context: $(this),
-	                method: 'POST',
+    function (OpenOrchestraCss) {
+        $(document).ready(function(){
+            $('form[name="Contact"').submit(function(event) {
+                event.preventDefault();
+                var data = $(this).serialize();   
+                $.ajax({
+                    url: $(this).attr('action'),
+                    context: $(this),
+                    method: 'POST',
                     data: data,
                     success: function(response){
-                    	$(this).html($('form', $(response)).html());
+                        $(this).html($('form', $(response)).html());
                     }
-	            });
+                });
             });
         });
     }
