68ada84ced6bcf814270e4e93c9e249002e48853
Amaury Lavieille <amaury.lavieille@gmail.com>
2016-08-23 17:48:08 +0200

    fix ie11 slideshow (#238) (#240)

--- DisplayBundle/Resources/public/libs/openOrchestraCss.js
+++ DisplayBundle/Resources/public/libs/openOrchestraCss.js
@@ -3,13 +3,13 @@ define(
     function() {
         return {
             load:function(cssFilesArray){
-                for (cssFile of cssFilesArray) {
+                cssFilesArray.forEach(function (cssFile) {
                     var link = document.createElement("link");
                     link.type = "text/css";
                     link.rel = "stylesheet";
                     link.href = cssFile;
                     document.getElementsByTagName("head")[0].appendChild(link);
-                }
+                });
             }
         };
     }
